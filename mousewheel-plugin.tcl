2fd228a64c2d41912990f7e1690c02290a261d3b
Joel Matthys <joel@matthysmusic.com>
2015-10-29 21:58:40 -0500

    initial commit

---
+++ mousewheel-plugin.tcl
@@ -0,0 +1,18 @@
+# META NAME mousewheel-plugin
+# META DESCRIPTION reads the mousewheel and sends it to the receive object mousewheel
+# META AUTHOR <Joel Matthys> joel@matthysmusic.com
+
+bind all <MouseWheel>       {pdsend "mousewheel %D"}
+bind all <Shift-MouseWheel> {pdsend "mousewheel %D"}
+if {[tk windowingsystem] eq "x11"} {
+    # Support for mousewheels on Linux/Unix commonly comes through
+    # mapping the wheel to the extended buttons.
+    bind all <Button-4> \
+            {event generate [focus -displayof %W] <MouseWheel> -delta  1}
+        bind all <Button-5> \
+            {event generate [focus -displayof %W] <MouseWheel> -delta -1}
+        bind all <Shift-Button-4> \
+            {event generate [focus -displayof %W] <Shift-MouseWheel> -delta  1}
+        bind all <Shift-Button-5> \
+            {event generate [focus -displayof %W] <Shift-MouseWheel> -delta -1}
+}
